9ea9fe6de1dafff389e33517463d7f83c82ecf6b
varvara <varia.mazurovaa@mail.ru>
2022-03-19 20:57:55 +0300

    на верхней панели отображается название фрагментов на русском

--- app/src/main/java/com/example/insense/ui/main_activity/MainActivity.java
+++ app/src/main/java/com/example/insense/ui/main_activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.insense.ui.main_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
@@ -9,6 +10,7 @@ import androidx.lifecycle.Observer;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,22 +19,49 @@ import com.example.insense.ui.fragments.Login.LoginViewModel;
 
 public class MainActivity extends AppCompatActivity {
     LoginViewModel viewModel = new LoginViewModel();
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
        NavGraph navGraph = navController.getNavInflater().inflate(R.navigation.nav_graph);
        navGraph.setStartDestination(R.id.starterFragment);
-       viewModel.authenticationState.observe(this,
+        NavController finalNavController = navController;
+        viewModel.authenticationState.observe(this,
                authenticationState ->{
            if(authenticationState == LoginViewModel.AuthenticationState.AUTHENTICATED) {
-               navController.navigate(R.id.mainFragment);
+               finalNavController.navigate(R.id.mainFragment);
            }else{
-               navController.navigate(R.id.loginFragment);
+               finalNavController.navigate(R.id.loginFragment);
            }
        });
+        //  4 строчки ниже отвечают за отображения названия фрагмента вверху экрана
        navController.setGraph(navGraph);
+        NavHostFragment navhost = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController =  navhost.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,  navController);
+    }
+    //метод для нажатия назад в верзнем левом углу
+    @Override
+    public boolean onSupportNavigateUp() {
+        if(navController.navigateUp()){
+            return true;
+        }
+        else{
+            return super.onSupportNavigateUp();
+        }
+
+    }
+    ///?????
+    @Override
+    public void onBackPressed()
+    {
+
+        super.onBackPressed();  // optional depending on your needs
     }
 }
