932253aff2c85aaf01f4dd0d65b22f619dc19053
varvara <varia.mazurovaa@mail.ru>
2022-05-01 22:41:36 +0300

    commit

--- app/src/main/java/com/example/insense/repository/OccupationRepository.java
+++ app/src/main/java/com/example/insense/repository/OccupationRepository.java
@@ -22,41 +22,67 @@ public class OccupationRepository {
 
         ArrayList<Occupation> occupationList = new ArrayList<Occupation>() {
         };
+
+
+//МУЗЫКА!
         Occupation piano = new Occupation();
-        piano.category = "хобби";
+        piano.category = "музыка";
         piano.name = "Игра на пианино";
         piano.description = "Изучение нот, запоминание мелодий";
         occupationList.add(piano);
 
         Occupation guitar = new Occupation();
-        guitar.category = "хобби";
+        guitar.category = "музыка";
         guitar.name = "Игра на гитаре";
         guitar.description = "Перебирание струн";
         occupationList.add(guitar);
 
+//ХОББИ!
+        Occupation draw = new Occupation();
+        draw.category = "хобби";
+        draw.name = "Рисование";
+        draw.description = "рисование";
+        occupationList.add(draw);
+
+        Occupation reading = new Occupation();
+        reading.category = "хобби";
+        reading.name = "Чтение";
+        reading.description = "чтение";
+        occupationList.add(reading);
+
+        Occupation computer_games = new Occupation();
+        computer_games.category = "хобби";
+        computer_games.name = "Компьютерные игры";
+        computer_games.description = "играть в комп";
+        occupationList.add(computer_games);
+
+        Occupation needlework = new Occupation();
+        needlework.category = "хобби";
+        needlework.name = "Рукоделие";
+        needlework.description = "делать что-то совими руками";
+        occupationList.add(needlework);
+
+
+//СПОРТ
         Occupation workout = new Occupation();
         workout.category = "спорт";
-        workout.name = "разминка";
+        workout.name = "Разминка";
         workout.description = "Легкая активность";
         occupationList.add(workout);
 
         Occupation run = new Occupation();
         run.category = "спорт";
-        run.name = "пробежка";
+        run.name = "Пробежка";
         run.description = "лоооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооо";
         occupationList.add(run);
 
         Occupation tennis = new Occupation();
         tennis.category = "спорт";
-        tennis.name = "игра в теннис";
+        tennis.name = "Теннис";
         tennis.description = "LOL! WHAT?!";
         occupationList.add(tennis);
 
-        Occupation temp = new Occupation();
-        temp.category = "спорт";
-        temp.name = "игра в теннис";
-        temp.description = "LOL! WHAT?!";
-        occupationList.add(temp);
+
 
 
 
@@ -66,6 +92,17 @@ public class OccupationRepository {
                 OccupationDB.class, "database").fallbackToDestructiveMigration().allowMainThreadQueries().build();
         occupationDAO = db.occupationDAO();
         occupationDAO.insertAll(occupationList);
+    }
+
+    public List<String> get_all_occupations(String categ){
+        List<Occupation> all;
+
+        all = occupationDAO.loadOccupationByCategoriesName(categ);
+        List<String> all_occup = new ArrayList<>();
+        for (int i = 0; i < all.size(); i++) {
+            all_occup.add(all.get(i).name);
+        }
+        return all_occup;
     }
     public List<String> occupation_by_category(String categ){
         List<Occupation> all;
--- app/src/main/java/com/example/insense/ui/fragments/Category/CategoryFragment.java
+++ app/src/main/java/com/example/insense/ui/fragments/Category/CategoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.insense.ui.fragments.Category;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,44 +11,49 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.insense.R;
 import com.example.insense.application.App;
-import com.example.insense.databinding.FragmentCategoriesBinding;
 import com.example.insense.databinding.FragmentCategoryBinding;
 import com.example.insense.repository.ActivityRepository;
-import com.example.insense.repository.CategoryRepository;
 import com.example.insense.repository.OccupationRepository;
-import com.example.insense.repository.room.categoryDB.Category;
-import com.example.insense.repository.room.categoryDB.CategoryDAO;
-import com.example.insense.repository.room.categoryDB.CategoryDB;
-import com.example.insense.repository.room.occupationDB.Occupation;
+import com.example.insense.repository.room.activityDB.Activity;
 import com.example.insense.repository.room.occupationDB.OccupationDAO;
+import com.example.insense.ui.main_activity.MainActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class CategoryFragment extends Fragment {
     private FragmentCategoryBinding fragmentCategoryBinding;
 
-    /*CategoryDB db = App.getInstance().getDatabase_category();*/
-    /*CategoryDAO categoryDAO = db.user_categ();*/
-    OccupationRepository repository = App.instance.getOccupationRepository();
-    OccupationDAO occupationDAO;
+
+    OccupationRepository repository_occupation = App.instance.getOccupationRepository();
+    ActivityRepository repository_activity = App.instance.getActivityRepository();
+    ExpandableListView expListView;
+    ExpandableListAdapter expListAdapter;
+    List<String> expListTitle;
+    Activity context_activity;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
+
 
         }
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,34 +69,78 @@ public class CategoryFragment extends Fragment {
         });
 
 
+
+
+
+
+
+
         return fragmentCategoryBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView tv = view.findViewById(R.id.textView_category);
 
 
         String text = getArguments().getString("arg1");
+        TextView tv = view.findViewById(R.id.textView_category);
         tv.setText(text);
-        List<String> all_ocuppations;
-        List<String> all_descriptions;
+        HashMap<String, List<String>> map_of_occupations_activities = new HashMap<>();
+        List<String> all_occupations = repository_occupation.get_all_occupations(text);
+        for (int i = 0; i < all_occupations.size(); i++) {
 
-
-        all_ocuppations = repository.occupation_by_category(text);
-        all_descriptions = repository.discription_of_occupation(text);
-        for (int i = 0; i < all_ocuppations.size(); i++) {
-            LinearLayout layout = (LinearLayout) view.findViewById(R.id.linear_layout_occupations);
-            View view1 = getLayoutInflater().inflate(R.layout.one_occupation_sample, null, false);
-            TextView textView = (TextView) view1.findViewById(R.id.button_occupation);
-            TextView textView1 = (TextView) view1.findViewById(R.id.textView_description_of_occupation);
-            textView.setText(all_ocuppations.get(i));
-            textView1.setText(all_descriptions.get(i));
-            layout.addView(view1);
-
-
+            List<String> all_activities = repository_activity.activity_by_category(all_occupations.get(i));
+            map_of_occupations_activities.put(all_occupations.get(i), all_activities);
         }
+
+        expListTitle = new ArrayList<>(map_of_occupations_activities.keySet());
+
+
+        expListView = (ExpandableListView) view.findViewById(R.id.expListView);
+
+
+
+        all_occupations = repository_occupation.occupation_by_category(text);
+
+        expListAdapter = new ListAdapter(getContext(), context_activity, all_occupations, map_of_occupations_activities, CategoryFragment.this);
+        expListView.setAdapter(expListAdapter);
+/*        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expListTitle.get(groupPosition) + " Список раскрыт.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expListTitle.get(groupPosition) + " Список скрыт.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });*/
+       /* expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expListTitle.get(groupPosition)
+                                + " : " + map_of_occupations_activities.get(expListTitle.get(groupPosition))
+                                .get(childPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });*/
+
+
+
+
+
+
 
     }
 }
